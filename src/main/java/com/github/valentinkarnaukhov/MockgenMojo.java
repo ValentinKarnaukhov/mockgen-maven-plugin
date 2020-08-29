052accb7310c24e70950f2f21659c5fd8a2b3d94
Valentin <valentine.karnaukhov@gmail.com>
2020-08-29 20:02:21 +0300

    init commit

---
+++ src/main/java/com/github/valentinkarnaukhov/MockgenMojo.java
@@ -0,0 +1,11 @@
+package com.github.valentinkarnaukhov;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+public class MockgenMojo extends AbstractMojo {
+
+    public void execute() throws MojoExecutionException {
+
+    }
+}
